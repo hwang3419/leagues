a9e7dd2275e915874db0fd2445a4358b577d5897
hwang3419 <hwang.iit@gmail.com>
2014-07-17 13:55:06 -0500

    settings update

--- m10/m10/settings.py
+++ m10/m10/settings.py
@@ -1,5 +1,5 @@
 # Django settings for m10 project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -15,18 +15,18 @@ MANAGERS = ADMINS
 #   Database Name: m10
 
 #Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'm10',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'adminsGdywJN',
-        'PASSWORD': 'L8X41ByzqFzi',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+if ON_OPENSHIFT:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'm10',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'adminsGdywJN',
+            'PASSWORD': 'L8X41ByzqFzi',
+            'HOST': os.environ['OPENSHIFT_DB_HOST'],                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'PORT': os.environ['OPENSHIFT_DB_PORT'],                      # Set to empty string for default.
+        }
     }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
